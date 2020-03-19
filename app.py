0618781ffea9a52c13233885067035941be0555d
Stoica Tedy <stoicatedy@gmail.com>
2020-03-19 12:19:50 +0200

    added basic index

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ SITES = ['pbinfo', 'infoarena', 'all']
 @app.route('/')
 @app.route('/index')
 def index():
-    data = query("Tedyst", "pbinfo")
-    return render_template('index.html', problems=data)
+    return render_template('index.html', SITES=SITES)
 
 
 @app.route('/api')
@@ -30,3 +29,20 @@ def api():
         status=200,
         mimetype='application/json'
     )
+
+
+@app.route('/search')
+def search():
+    user = request.args.get('user')
+    site = request.args.get('site')
+    if site not in SITES:
+        return render_template('404.html')
+    data = query(user, site)
+    if site == "all":
+        data = sorted(data, key=operator.itemgetter("data"))
+    return app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+
